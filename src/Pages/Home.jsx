1b7185035ed95e8c7dca2a3024edd9c01cd20893
Ajharahmed3218 <nuhash3218@gmail.com>
2024-09-06 06:37:03 +0600

    Added: Statistics of service recipients section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
         <div>
             <Header />
 
-            {/*  */}
+            {/* বায়োডাটা তৈরি  */}
             <section className="text-center lg:my-[12rem] ">
                 <h2 className="text-[#522b79] text-[3rem] font-bold"><span className="text-[#e92f83]">অর্ধেকদ্বীনে</span> সম্পূর্ণ বিনামূল্যে বায়োডাটা তৈরি করা যায়</h2>
 
@@ -18,6 +18,94 @@ const Home = () => {
                     </a>
                 </div>
             </section>
+
+            {/* সেবা গ্রহীতার পরিসংখ্যান */}
+            <section className=" lg:my-[12rem]">
+                <h2 className="text-[#522b79] text-center text-[3rem] font-bold"><span className="text-[#e92f83]">অর্ধেকদ্বীনে</span>  সেবা গ্রহীতার পরিসংখ্যান</h2>
+
+
+                <div className="flex justify-center mt-20 gap-6">
+                    <div className="flex flex-col bg-box w-[300px] justify-center items-center p-[50px] rounded-3xl gap-4 hover:shadow-lg"
+                        style={{ transition: 'box-shadow .35s ease 0s' }}>
+                        <img className="w-[130px]" src="couple.svg" alt="couple" />
+                        <h4>সর্বমোট পাত্র-পাত্রীর বায়োডাটা</h4>
+                        <h3 className="text-[3rem] text-[#000] leading-[1.2]">
+                            <span>6,696</span>
+                        </h3>
+                    </div>
+
+                    <div className="flex flex-col bg-box w-[300px] justify-center items-center p-[50px] rounded-3xl gap-4 hover:shadow-lg"
+                        style={{ transition: 'box-shadow .35s ease 0s' }}>
+                        <img className="w-[75px]" src="male.svg" alt="couple" />
+                        <h4>সর্বমোট পাত্রের বায়োডাটা</h4>
+                        <h3 className="text-[3rem] text-[#000] leading-[1.2]">
+                            <span>2,796</span>
+                        </h3>
+                    </div>
+                    <div className="flex flex-col bg-box w-[300px] justify-center items-center p-[50px] rounded-3xl gap-4 hover:shadow-lg"
+                        style={{ transition: 'box-shadow .35s ease 0s' }}>
+                        <img className="w-[75px]" src="female.svg" alt="couple" />
+                        <h4>সর্বমোট পাত্রীর বায়োডাটা</h4>
+                        <h3 className="text-[3rem] text-[#000] leading-[1.2]">
+                            <span>3,891</span>
+                        </h3>
+                    </div>
+                    <div className="flex flex-col bg-box w-[300px] justify-center items-center p-[50px] rounded-3xl gap-4 hover:shadow-lg"
+                        style={{ transition: 'box-shadow .35s ease 0s' }}>
+                        <img className="w-[100px]" src="married.svg" alt="couple" />
+                        <h4>সর্বমোট সফল বিবাহ</h4>
+                        <h3 className="text-[3rem] text-[#000] leading-[1.2]">
+                            <span>1,960</span>
+                        </h3>
+                    </div>
+
+                </div>
+
+            </section>
+
+            {/* সেবা গ্রহীতার পরিসংখ্যান
+            <section className=" lg:my-[12rem]">
+                <h2 className="text-[#522b79] text-center text-[3rem] font-bold"><span className="text-[#e92f83]">অর্ধেকদ্বীনে</span>   যেভাবে কাজ করে</h2>
+
+
+                <div className="flex justify-center mt-20 gap-6">
+                    <div className="flex flex-col bg-box w-[300px] justify-center items-center p-[50px] rounded-3xl gap-4 hover:shadow-lg"
+                        style={{ transition: 'box-shadow .35s ease 0s' }}>
+                        <img className="w-[130px]" src="couple.svg" alt="couple" />
+                        <h4>সর্বমোট পাত্র-পাত্রীর বায়োডাটা</h4>
+                        <h3 className="text-[3rem] text-[#000] leading-[1.2]">
+                            <span>6,696</span>
+                        </h3>
+                    </div>
+
+                    <div className="flex flex-col bg-box w-[300px] justify-center items-center p-[50px] rounded-3xl gap-4 hover:shadow-lg"
+                        style={{ transition: 'box-shadow .35s ease 0s' }}>
+                        <img className="w-[75px]" src="male.svg" alt="couple" />
+                        <h4>সর্বমোট পাত্রের বায়োডাটা</h4>
+                        <h3 className="text-[3rem] text-[#000] leading-[1.2]">
+                            <span>2,796</span>
+                        </h3>
+                    </div>
+                    <div className="flex flex-col bg-box w-[300px] justify-center items-center p-[50px] rounded-3xl gap-4 hover:shadow-lg"
+                        style={{ transition: 'box-shadow .35s ease 0s' }}>
+                        <img className="w-[75px]" src="female.svg" alt="couple" />
+                        <h4>সর্বমোট পাত্রীর বায়োডাটা</h4>
+                        <h3 className="text-[3rem] text-[#000] leading-[1.2]">
+                            <span>3,891</span>
+                        </h3>
+                    </div>
+                    <div className="flex flex-col bg-box w-[300px] justify-center items-center p-[50px] rounded-3xl gap-4 hover:shadow-lg"
+                        style={{ transition: 'box-shadow .35s ease 0s' }}>
+                        <img className="w-[100px]" src="married.svg" alt="couple" />
+                        <h4>সর্বমোট সফল বিবাহ</h4>
+                        <h3 className="text-[3rem] text-[#000] leading-[1.2]">
+                            <span>1,960</span>
+                        </h3>
+                    </div>
+
+                </div>
+
+            </section> */}
         </div>
     );
 };
